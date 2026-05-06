676dcf38b65a900b4ad9346b576bfbee447ea739
Samuel Gassman <sgassman2006@gmail.com>
2026-05-06 14:49:53 -0400

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> numSet = new HashSet<Integer>();
+
+        int i = 0;
+        boolean dupl = false;
+        while (i < nums.length && !dupl) {
+            
+            if (numSet.contains(nums[i])) {
+                dupl = true;
+            }
+            numSet.add(nums[i]);
+
+            i++;
+        }
+
+        return dupl;
+    }
+}
