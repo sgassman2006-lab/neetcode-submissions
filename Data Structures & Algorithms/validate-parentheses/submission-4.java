9e3ea381bfd33717e80d263d322c90b735b4078a
Samuel Gassman <sgassman2006@gmail.com>
2026-05-07 17:21:29 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) {
+            return false;
+        }
+        Stack<Character> check = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++) {
+            char current = s.charAt(i);
+            if (check.empty()) {
+                if (current == '}' || current == ']' || current == ')') 
+                    return false;
+                check.push(current);
+            } else {
+                char c = check.peek();
+                if (current == '{' || current == '[' || current == '(')
+                    check.push(current);
+                else if (c == '{' && current != '}' || c == '[' && current != ']' || c == '(' && current != ')')
+                    return false;
+                else
+                    check.pop();
+            }
+        }
+        if (check.empty())
+            return true;
+        return false;
+    }
+}
