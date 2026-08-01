bfef73b89b130df8d0da5c78edcf21c4782c8694
Samuel Gassman <sgassman2006@gmail.com>
2026-07-31 19:16:10 -0500

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        letters = {}
+        for let in s:
+            if let in letters:
+                letters[let] += 1
+            else:
+                letters[let] = 1
+        for let in t:
+            if let not in letters:
+                return False
+            elif letters[let] > 1:
+                letters[let] -= 1
+            else:
+                letters.pop(let)
+        return not letters
